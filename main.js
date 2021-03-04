85d499425f2dbecbd769a866ac591129c719da62
Colin Peters <colinthedev@gmail.com>
2021-03-03 20:03:16 -0500

    built img gallery for tablets

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-//Img Gallery
-var slideIndex = 0;
-showSlides();
+// //Img Gallery
+// var slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("galleryWrapper__img");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if(slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); //Change image every 4 seconds
-}
+// function showSlides() {
+//     var i;
+//     var slides = document.getElementsByClassName("galleryWrapper__img");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if(slideIndex > slides.length) {slideIndex = 1}
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 4000); //Change image every 4 seconds
+// }
+
+
